2bcd15b4d69843b12d5a877c0d7ff0fc91b692a3
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-06-16 10:23:06 +0800

    1

---
+++ src/main/java/chenjunfu2/earlycompat/util/EasyPlaceExtraProtocolHelper.java
@@ -0,0 +1,14 @@
+package chenjunfu2.earlycompat.util;
+
+public class EasyPlaceExtraProtocolHelper
+{
+	public static boolean isExtraProtocol(int protocolValue)
+	{
+		return (protocolValue & 0b0000_1000) == 0b0000_1000;//判断bit3是否为1，是的话则为扩展协议
+	}
+	
+	public static byte getExtraProtocolRawValue(int protocolValue)
+	{
+		return (byte)(((protocolValue & 0b1111_0000) >>> 1) | (protocolValue & 0b0000_0111));//摘除bit3，拼接剩余位，一共剩余7bit可用
+	}
+}

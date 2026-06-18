4d8554c7812aad0b734ce4e6ec60b5d22d911ee5
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-06-18 19:15:42 +0800

    fix1

--- src/main/java/chenjunfu2/earlycompat/mixin/EasyPlaceFix/Vanilla/Protocol/DoorBlockMixin_VanillaProtocolCompat.java
+++ src/main/java/chenjunfu2/earlycompat/mixin/EasyPlaceFix/Vanilla/Protocol/DoorBlockMixin_VanillaProtocolCompat.java
@@ -4,6 +4,7 @@ import chenjunfu2.earlycompat.util.BlockProtocolStateAdapter;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.block.enums.DoorHinge;
+import net.minecraft.util.math.Direction;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -13,20 +14,24 @@ public abstract class DoorBlockMixin_VanillaProtocolCompat implements BlockProto
 	@Override
 	public int earlycompat$toProtocolValue(int protocolValue, BlockState fromState)
 	{
+		int facingOrdinal = fromState.get(DoorBlock.FACING).ordinal() - 2;
 		int hingeOrdinal = fromState.get(DoorBlock.HINGE).ordinal();
 		boolean isOpen = fromState.get(DoorBlock.OPEN);
 		int bits =
-			(hingeOrdinal & 0b0000_0001) << 4 |
-				(isOpen ? 0b0010_0000 : 0b0000_0000);
-		return protocolValue | bits;
+			(facingOrdinal & 0b0000_0011) |
+				(hingeOrdinal & 0b0000_0001) << 2 |
+				(isOpen ? 0b0000_1000 : 0b0000_0000);
+		return bits;
 	}
 	
 	@Override
 	public @NotNull BlockState earlycompat$fromProtocolValue(int extraProtocolValue, BlockState fromState)
 	{
-		int hingeOrdinal = (extraProtocolValue & 0b0001_0000) >>> 4;//0~1
-		boolean isOpen = (extraProtocolValue & 0b0010_0000) == 0b0010_0000;
+		int facingOrdinal = (extraProtocolValue & 0b0000_0011) + 2;
+		int hingeOrdinal = (extraProtocolValue & 0b0000_0100) >>> 2;//0~1
+		boolean isOpen = (extraProtocolValue & 0b0000_1000) == 0b0000_1000;
 		return fromState
+			.with(DoorBlock.FACING, Direction.values()[facingOrdinal])
 			.with(DoorBlock.HINGE, DoorHinge.values()[hingeOrdinal])
 			.with(DoorBlock.OPEN, isOpen);
 	}
@@ -34,6 +39,6 @@ public abstract class DoorBlockMixin_VanillaProtocolCompat implements BlockProto
 	@Override
 	public @NotNull ProtocolType earlycompat$getProtocolType()
 	{
-		return ProtocolType.ADDED;
+		return ProtocolType.REPLACE;
 	}
 }
--- src/main/java/chenjunfu2/earlycompat/mixin/EasyPlaceFix/Vanilla/Protocol/TrapdoorBlockMixin_VanillaProtocolCompat.java
+++ src/main/java/chenjunfu2/earlycompat/mixin/EasyPlaceFix/Vanilla/Protocol/TrapdoorBlockMixin_VanillaProtocolCompat.java
@@ -4,6 +4,7 @@ import chenjunfu2.earlycompat.util.BlockProtocolStateAdapter;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.TrapdoorBlock;
 import net.minecraft.block.enums.BlockHalf;
+import net.minecraft.util.math.Direction;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -13,20 +14,24 @@ public abstract class TrapdoorBlockMixin_VanillaProtocolCompat implements BlockP
 	@Override
 	public int earlycompat$toProtocolValue(int protocolValue, BlockState fromState)
 	{
+		int facingOrdinal = fromState.get(TrapdoorBlock.FACING).ordinal() - 2;
 		int halfOrdinal = fromState.get(TrapdoorBlock.HALF).ordinal();
 		boolean isOpen = fromState.get(TrapdoorBlock.OPEN);
 		int bits =
-			(halfOrdinal & 0b0000_0001) << 4 |
-			(isOpen ? 0b0010_0000 : 0b0000_0000);
-		return protocolValue | bits;
+			(facingOrdinal & 0b0000_0011) |
+			(halfOrdinal & 0b0000_0001) << 2 |
+			(isOpen ? 0b0000_1000 : 0b0000_0000);
+		return bits;
 	}
 	
 	@Override
 	public @NotNull BlockState earlycompat$fromProtocolValue(int extraProtocolValue, BlockState fromState)
 	{
-		int halfOrdinal = (extraProtocolValue & 0b0001_0000) >>> 4;//0~1
-		boolean isOpen = (extraProtocolValue & 0b0010_0000) == 0b0010_0000;
+		int facingOrdinal = (extraProtocolValue & 0b0000_0011) + 2;
+		int halfOrdinal = (extraProtocolValue & 0b0000_0100) >>> 2;//0~1
+		boolean isOpen = (extraProtocolValue & 0b0000_1000) == 0b0000_1000;
 		return fromState
+			.with(TrapdoorBlock.FACING, Direction.values()[facingOrdinal])
 			.with(TrapdoorBlock.HALF, BlockHalf.values()[halfOrdinal])
 			.with(TrapdoorBlock.OPEN, isOpen);
 	}
@@ -34,6 +39,6 @@ public abstract class TrapdoorBlockMixin_VanillaProtocolCompat implements BlockP
 	@Override
 	public @NotNull ProtocolType earlycompat$getProtocolType()
 	{
-		return ProtocolType.ADDED;
+		return ProtocolType.REPLACE;
 	}
 }

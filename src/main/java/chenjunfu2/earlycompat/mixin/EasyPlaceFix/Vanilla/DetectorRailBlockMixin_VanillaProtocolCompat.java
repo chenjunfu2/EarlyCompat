0eaf290d8a97c96bbda07fe08416eb02ad1111ce
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-07-30 15:09:45 +0800

    铁轨无更新修复，追加

--- src/main/java/chenjunfu2/earlycompat/mixin/EasyPlaceFix/Vanilla/DetectorRailBlockMixin_VanillaProtocolCompat.java
+++ src/main/java/chenjunfu2/earlycompat/mixin/EasyPlaceFix/Vanilla/DetectorRailBlockMixin_VanillaProtocolCompat.java
@@ -1,5 +1,6 @@
 package chenjunfu2.earlycompat.mixin.EasyPlaceFix.Vanilla;
 
+import chenjunfu2.earlycompat.accessor.PlaceStateAccessor;
 import chenjunfu2.earlycompat.util.BlockProtocolStateAdapter;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.DetectorRailBlock;
@@ -9,6 +10,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 
+import static chenjunfu2.earlycompat.config.EarlyCompatConfigs.EASY_PLACE_RAIL_BLOCK_NO_SHAPE_UPDATE;
+
 @Mixin(DetectorRailBlock.class)
 public abstract class DetectorRailBlockMixin_VanillaProtocolCompat implements BlockProtocolStateAdapter
 {
@@ -16,13 +19,26 @@ public abstract class DetectorRailBlockMixin_VanillaProtocolCompat implements Bl
 	public int earlycompat$toProtocolValue(int protocolValue, BlockState fromState)
 	{
 		int shapeOrdinal = fromState.get(DetectorRailBlock.SHAPE).ordinal();
-		return shapeOrdinal & 0b0000_0111;
+		boolean noUpdate = EASY_PLACE_RAIL_BLOCK_NO_SHAPE_UPDATE();
+		
+		int bits =
+			(shapeOrdinal & 0b0000_0111) |
+			(noUpdate ? 0b0000_1000 : 0b0000_0000);
+		
+		return bits;
 	}
 	
 	@Override
 	public @Nullable BlockState earlycompat$fromProtocolValue(int extraProtocolValue, BlockState fromState, ItemPlacementContext context)
 	{
 		int shapeOrdinal = (extraProtocolValue & 0b0000_0111) % 6;//0~5 6种状态
+		boolean noUpdate = (extraProtocolValue & 0b0000_1000) == 0b0000_1000;
+		
+		if(noUpdate && (fromState.getBlock().asItem() instanceof PlaceStateAccessor placeStateAccessor))
+		{
+			placeStateAccessor.earlycompat$setPlaceFlag(PlaceStateAccessor.easyPlaceRailBlockNoShapeUpdate);
+		}
+		
 		return fromState
 			.with(DetectorRailBlock.SHAPE, RailShape.values()[shapeOrdinal]);
 	}
